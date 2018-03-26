36fa122c0a1aeda4b9347b32e1f8d8b96f0ac805
Julika Maiste <julimai@users.noreply.github.com>
2018-03-26 09:29:35 +0300

    automaattestid

--- src/main/java/seitsmes/Alustus.java
+++ src/main/java/seitsmes/Alustus.java
@@ -2,5 +2,7 @@ package seitsmes;
 public class Alustus{
   public static void main(String[] args){
     System.out.println("algus");
+	System.out.println(Funktsioonid.keskmine(3, 7));
+	System.out.println(Funktsioonid.korrutus(4, 10));
   }
 }
--- src/main/java/seitsmes/Funktsioonid.java
+++ src/main/java/seitsmes/Funktsioonid.java
@@ -1,6 +1,10 @@
 package seitsmes;
 public class Funktsioonid{
-   public static int korruta(int arv1, int arv2){
+   public static int keskmine(int arv1, int arv2){
+      return (arv1+arv2)/2;
+   }
+   public static int korrutus(int arv1, int arv2){
       return arv1*arv2;
    }
+  
 }
